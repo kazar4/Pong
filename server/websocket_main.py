10444f506141ab7124084b0b90c496e396e26aa3
Kazen Gallman <kazar444@icloud.com>
2021-12-24 02:35:48 -0500

    added example code for python websockets and changed name of react folder

---
+++ server/websocket_main.py
@@ -0,0 +1,11 @@
+import logging
+from websocket_server import WebsocketServer
+
+# pip install websocket-server
+
+def new_client(client, server):
+	server.send_message_to_all("Hey all, a new client has joined us")
+
+server = WebsocketServer(host='127.0.0.1', port=13254, loglevel=logging.INFO)
+server.set_fn_new_client(new_client)
+server.run_forever()
